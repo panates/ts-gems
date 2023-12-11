abdcb49098bf0ac676a1259c8e260ac308809e6d
Eray Hanoğlu <e.hanoglu@panates.com>
2023-12-12 00:28:56 +0300

    Fixed infinite deep error

--- lib/deep-remove-nulls.d.ts
+++ lib/deep-remove-nulls.d.ts
@@ -1,20 +1,20 @@
+import { DeepOmitNever, HighDeepOmitNever } from './deep-omit.js';
 import { IfNoDeepValue } from './helpers.js';
-import { OmitNever } from './omit.js';
 
 /**
  * Remove null from properties deeply
  */
-export type DeepRemoveNulls<T> = _DeepRemoveNulls<T>;
+export type DeepRemoveNulls<T> = DeepOmitNever<_DeepRemoveNulls<T>>;
 type _DeepRemoveNulls<T> =
     IfNoDeepValue<T> extends true ? T
-        : OmitNever<{ [P in keyof T]: _DeepRemoveNulls<Exclude<T[P], null>> }>;
+        : { [P in keyof T]: _DeepRemoveNulls<Exclude<T[P], null>> };
 
 
 /**
  * Remove null from properties deeply including arrays
  */
-export type HighDeepRemoveNulls<T> = _HighDeepRemoveNulls<T>;
+export type HighDeepRemoveNulls<T> = HighDeepOmitNever<_HighDeepRemoveNulls<T>>;
 type _HighDeepRemoveNulls<T> =
     T extends (infer U)[] ? _HighDeepRemoveNulls<U>[]
         : IfNoDeepValue<T> extends true ? T
-            : OmitNever<{ [P in keyof T]: _HighDeepRemoveNulls<Exclude<T[P], null>> }>;
+            : { [P in keyof T]: _HighDeepRemoveNulls<Exclude<T[P], null>> };
